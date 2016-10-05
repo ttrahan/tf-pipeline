fb472c37f26c3365012456b2417b122f34d33543
Tom Trahan <tom@shippable.com>
2016-10-04 21:14:53 -0700

    process state into runSh job

--- runTerraform.sh
+++ runTerraform.sh
@@ -1,11 +1,68 @@
 #! /bin/bash
 
-apt-get update
-apt-get install curl unzip
+# Install tools
+sudo apt-get update && apt-get install curl jq unzip
 
 FILE_NAME=terraform_0.7.4_linux_amd64.zip
 
-curl https://releases.hashicorp.com/terraform/0.7.4/$FILE_NAME
-unzip $FILE_NAME -d .
+# Extract previous state
+echo -e "\n*** extracting previous state for this job ***"
+get_previous_statefile() {
+  local previous_statefile_location="/build/previousState/tfDeploy.env"
+  if [ -f "$previous_statefile_location" ]; then
+    . $previous_statefile_location
+    echo 'restored '$(compgen -A variable | wc -l)' environment variables'
+  else
+    echo "no previous statefile exists"
+  fi
+}
+get_previous_statefile
 
-terraform apply
+# Extract integration data
+echo -e "\n*** extracting AWS integration information ***"
+get_aws_integration() {
+  local INTEGRATION_FILE="./IN/integration-aws/integration.env"
+  if [ -f "$INTEGRATION_FILE" ]; then
+    . $INTEGRATION_FILE
+    AWS_ACCESS_KEY_ID=$aws_access_key_id
+    AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
+    echo "loaded integration file"
+  else
+    echo "no integration file exists"
+  fi
+}
+get_aws_integration
+
+# Extract params data
+echo -e "\n*** extracting params information ***"
+get_params() {
+  local PARAMS_FILE="./IN/params-tfScripts/version.json"
+  if [ -f "$PARAMS_FILE" ]; then
+    PARAMS_VALUES=$(jq -r '.version.propertyBag.params' $PARAMS_FILE)
+    PARAMS_LENGTH=$(echo $PARAMS_VALUES | jq '. | length')
+    PARAMS_KEYS=$(echo $PARAMS_VALUES | jq '. | keys')
+    for (( i=0; i<$PARAMS_LENGTH; i++ )) do
+      PARAM_KEY=$(echo $PARAMS_KEYS | jq -r .[$i])
+      export $PARAM_KEY=$(echo $PARAMS_VALUES | jq -r .[\"$PARAM_KEY\"])
+    done
+    echo "loaded params file"
+  else
+    echo "no params file exists"
+  fi
+}
+get_params
+
+# Install Terraform
+echo -e "\n*** installing Terraform ***"
+install_terraform() {
+  curl https://releases.hashicorp.com/terraform/0.7.4/$FILE_NAME
+  unzip $FILE_NAME -d .
+}
+install_terraform
+
+# Provision infrastructure via scripts
+echo -e "\n*** provisioning infrastructure on AWS ***"
+provision_infra() {
+  terraform apply -var aws_access_key_id=$AWS_ACCESS_KEY_ID -var aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+}
+provision_infra
