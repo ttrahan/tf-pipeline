9bbb5b423ef8a5f2fbe6d47c08ff43d17bca5a1d
Tom Trahan <tom@shippable.com>
2016-10-04 22:30:55 -0700

    fixes subnet id

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "sampleInstances" {
   ami = "ami-0d729a60"
   availability_zone = "us-east-1b"
   instance_type = "t2.micro"
-  subnet_id = "demoPubSN0-0-1"
+  subnet_id = "subnet-84ea36cd"
   associate_public_ip_address = true
   source_dest_check = false
 
