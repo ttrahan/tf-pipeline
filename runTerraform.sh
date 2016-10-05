aaa887d93c438ecf262d3a55fc3354683b0488e4
Tom Trahan <tom@shippable.com>
2016-10-04 22:03:29 -0700

    fixes path to terraform.tf file

--- runTerraform.sh
+++ runTerraform.sh
@@ -65,6 +65,7 @@ get_params
 # Provision infrastructure via scripts
 echo -e "\n*** provisioning infrastructure on AWS ***"
 provision_infra() {
+  cd /build/IN/repo-tfScripts/gitRepo
   terraform apply -var aws_access_key_id=$AWS_ACCESS_KEY_ID -var aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 }
 provision_infra
