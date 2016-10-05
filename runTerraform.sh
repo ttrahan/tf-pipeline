a64c1c03000236c8ed4ecf956ee157b0ad467cc3
Tom Trahan <tom@shippable.com>
2016-10-04 22:38:44 -0700

    fixes location to copy terraform.tfstate file

--- runTerraform.sh
+++ runTerraform.sh
@@ -20,7 +20,7 @@ echo -e "\n*** extracting previous state for this job ***"
 get_previous_statefile() {
   local previous_statefile_location="/build/previousState/terraform.tfstate"
   if [ -f "$previous_statefile_location" ]; then
-    cp $previous_statefile_location $(pwd)
+    cp $previous_statefile_location /build/IN/repo-tfScripts/gitRepo
     echo 'restored previous statefile'
   else
     echo "no previous statefile exists"
